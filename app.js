20f606a4c5aacaa3f34d3ac4bc745f91a719e9da
polojerry <polojerry4@gmail.com>
2020-06-09 14:00:40 +0300

    Create user api done

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 require("dotenv").config();
 
-const express = require('express')
+const express = require('express');
 const app = express();
+const userRouter = require("./api/users/user.router");
 
-app.get("/api", (req, res)=>{
-    res.json({
-        success: 1,
-        message: "Api Working"
-
-    });
-});
+app.use(express.json())
+app.use("/api/users", userRouter); 
 
 app.listen(process.env.APP_PORT, ()=>{
-    console.log("Server Running on port: ",process.env.APP_PORT )
+    console.log("Server Running on port: ",process.env.APP_PORT );
 });
