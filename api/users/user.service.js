d4b6a66cc224c67b91a9d79c2d5fa85d278f07c3
polojerry <polojerry4@gmail.com>
2020-06-09 16:00:03 +0300

    Added getUsers, getUsersById, UpdateUser and deleteUser Services

--- api/users/user.service.js
+++ api/users/user.service.js
@@ -14,8 +14,67 @@ module.exports = {
                 if(error){
                     return callBack(error);
                 }
-                return callBack(null, results)
+                return callBack(null, results);
             }
-        )
+        );
+    },
+
+    getUsers: (data, callback)=>{
+        pool.query(
+            `SELECT firstName,lastName,email,password FROM user_details`,
+            [],
+            (err, res,fields)=>{
+                if(err){
+                    return callback(err);
+                }
+                return callback(null,res);
+            }
+        );
+    },
+
+    getUserById: (data, callback)=>{
+        pool.query(`SELECT firstName,lastName,email,password FROM user_details WHERE use_id =?`,
+        [data.user_id],
+        (err, res,fields)=>{
+            if(err){
+                return callback(err);
+            }
+
+            return callback(null,res[0]);
+        }
+        );
+    },
+
+    updateUser: (data, callBack)=>{
+        pool.query(`UPDATE user_details set firstName = ?,lastName = ?,email = ?, password WHERE use_id =?`,
+        [
+            data.firstName,
+                data.lastName,
+                data.email,
+                data.password,
+                data.user_id
+        ],
+        (err, res)=>{
+            if(err){
+                return callBack(err);
+            }
+
+            return callBack(null, res[0]);
+        }
+
+        );
+    },
+
+    deleteUser: (data, callBack)=>{
+        pool.query(`DELETE from user_details WHERE id=?`,
+        [data.user_id],
+        (err, res)=>{
+            if(err){
+                return callBack(err);
+            }
+
+            return callBack(null,res[0]);
+        }
+        );
     }
 }
